684307021a03a5d8db1e976c754d2430f28252cf
andreasb09 <andreasb09.kodehode@gmail.com>
2024-05-29 08:54:13 +0200

    base

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// import { useState } from 'react'
+import "./App.css";
+import Slider from "./components/cssSlider/cssSlider";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return <Slider />;
+}
+
+export default App;
---
+++ src/components/cssSlider/cssSlider.jsx
@@ -0,0 +1,11 @@
+import styles from './cssSlider.module.css';
+
+function Slider () {
+    return (
+        <div className={styles.container}>
+
+        </div>
+    );
+}
+
+export default Slider
