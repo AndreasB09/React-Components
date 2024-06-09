c925126d2719366f07b15a219531426907042bbe
AndreasB09 <andreasb09.kodehode@gmail.com>
2024-06-09 20:27:11 +0200

    working on header. added various images and favicon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,19 @@
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
+import tree from '../../assets/images/tree-icon.svg';
 
 function Header() {
     return (
         <header className={styles.header}>
-            <div className={styles.logo}></div>
+            <div className={styles.logo}>
+                <img src={tree} alt="Tree Logo" className={styles.treeLogo} />
+            </div>
             <div className={styles.btnContainer}>
                 <Link to="/">
-                    <button>Home</button>
+                    <button className={styles.navBtn}>Home</button>
                 </Link>
                 <Link to="/slider">
-                    <button>Slider</button>
+                    <button className={styles.navBtn}>Slider</button>
                 </Link>
             </div>
         </header>
