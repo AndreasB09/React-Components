b17251832c372775b0399567298891001174706d
AndreasB09 <andreasb09.kodehode@gmail.com>
2024-07-23 23:02:06 +0200

    cleanup homepage and header

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,15 @@
 import Layout from "../../components/Layout/Layout.jsx";
+import forestImage from "../../assets/images/home/forest.png";
+import styles from "./Home.module.css";
 
 function Home() {
     return (
         <Layout>
-            <h1>Hi</h1>
+            <div className={styles.imageContainer}>
+                <h1>Welcome</h1>
+                <p>This is an attempt to collect some components I&apos;ve been making in React.</p>
+                <img src={forestImage} alt="Lush green forest" className={styles.forestImage} />
+            </div>
         </Layout>
     );
 }
