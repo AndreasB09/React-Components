9836153b96049e6ae9d724e21c26a8edaacacf77
AndreasB09 <andreasb09.kodehode@gmail.com>
2024-07-31 18:51:31 +0200

    animation page and placeholder card

---
+++ src/pages/Animations/Animations.jsx
@@ -0,0 +1,16 @@
+import Layout from "../../components/Layout/Layout.jsx";
+import styles from './Animations.module.css';
+
+function Animations() {
+    return (
+        <Layout>
+            <div className={styles.animationContainer}>
+                <div className={styles.card}>
+
+                </div>
+            </div>
+        </Layout>
+    );
+}
+
+export default Animations;
